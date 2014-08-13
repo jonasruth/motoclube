186f8c88a76586e78b51fdddbf823edcefbb1978
Jonas Ruth <jonasruth@gmail.com>
2014-08-12 21:44:40 -0300

    Tabela de preços

---
+++ tabela_precos.php
@@ -0,0 +1,78 @@
+<?php
+require('header_inc.php');
+require('db_inc.php');
+require('aut_inc.php');
+
+$log = "\niniciando tabela preços - " . date("Y-m-d H:i:s", time());
+$log .= "\n\n".var_export($_REQUEST,true)."\n\n";
+
+if(isset($_REQUEST['usuario'])){
+
+	$retorno = '';
+
+	$result = $mysqli->query("select 
+			j.id as id,
+			k.nome_simples as motoclube,
+			l.titulo as evento,
+			date_format(l.datahora,'%d/%m/%Y') as data
+		from valor_praticado j 
+		inner join motoclube k on k.id = j.motoclube_id 
+		inner join evento l on l.id = j.evento_id
+		where j.evento_id=2");
+	$tabela = $result->fetch_object();
+	$result->close();
+
+	if($tabela){
+		$query = "select 
+			k.titulo as produto,
+			j.valor_unitario 
+			from valor_praticado_item j
+			inner join produto k on k.id = j.produto_id 
+			where j.valor_praticado_id = {$tabela->id}";
+
+		$result = $mysqli->query($query);
+
+		$tabela_itens = array();
+		while($item = $result->fetch_assoc()){
+			array_push(
+				$tabela_itens, array(
+					'nome'  => $item['produto'],
+					'preco' => $item['valor_unitario'],
+				)
+			);
+		}
+		$result->close();
+
+		$itens = array(
+			'cabecalho' => array(
+				'motoclube' => $tabela->motoclube,
+				'evento' => $tabela->evento,
+				'data' => $tabela->data,
+			),
+			'itens' => $tabela_itens,
+		);
+
+		$mysqli->close();
+		
+		$retorno = 'tabela_ok';
+		$json = array(
+			'retorno' => $retorno,
+			'itens'=>$itens
+		);
+		
+	}else{
+		$retorno = "tabela_nok";
+		$json = array(
+			'retorno' => $retorno,
+		);
+	}
+
+}else{
+
+	$retorno = "usuario_nok";
+	$json = array(
+		'retorno' => $retorno,
+	);
+}
+echo json_encode($json);
+file_put_contents('log.txt', $log);
