c4af812ad23f13bcb31e56954d44d0f984968366
jonasruth <jonasruth@gmail.com>
2014-08-14 22:25:50 +0000

    Iniciando esqueleto do CRUD

---
+++ admin/usuario_list.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<head>
+<meta charset="UTF-8"/>
+<style type="text/css">
+.form-line label{ display:inline-block; width:6em; }
+
+</style>
+
+</head>
+
+<h1>Manter Perfil</h1>
+
+<form>
+<div class="form-line">
+<label for="usuario">Usuário</label>
+<input id="" type="text" name="" value="" maxlength="" placeholder="" required=""/>
+</div>
+
+<div class="form-line">
+<label for="email">Email</label>
+<input id="" type="email" name="" value="" maxlength="" placeholder="" required=""/>
+</div>
+
+<div class="form-line">
+<label for="senha">Senha</label>
+<input id="" type="text" name="" value="" maxlength="" placeholder="" required=""/>
+</div>
+
+<div class="form-line">
+<label for="apelido">Apelido</label>
+<input id="" type="text" name="" value="" maxlength="" placeholder="" required=""/>
+</div>
+
+<div class="form-line">
+<label for="motoclube">Motoclube</label>
+<select id="motoclube" name="motoclube" required="true">
+<option value="1">Gárgulas Moto Clube</option>
+</select>
+</div>
+
+<div class="form-line">
+<label for="graduacao">Graduação</label>
+<select id="graduacao" name="graduacao" required="true">
+<option value="Convidado">Convidado</option>
+<option value="Prospect">Prospect</option>
+<option value="Meio Escudo">Meio Escudo</option>
+<option value="Escudado">Escudado</option>
+</select>
+</div>
+
+<label for=""></label>
+<input type="text" name="" value="" maxlength="" placeholder="" required=""/>
+</form>
+
+</body>
+</html>
