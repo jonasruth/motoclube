5b81c383944613dc1bb07778fc153d03b28bae77
Jonas Ruth <jonasruth@gmail.com>
2014-08-28 02:29:00 -0300

    Cadastro de usuários web

---
+++ admin/welcome.php
@@ -0,0 +1,81 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>MeuBandoApp!!</title>
+
+    <!-- Bootstrap core CSS -->
+   <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+   <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+   
+
+    <!-- Custom styles for this template -->
+    <link href="jumbotron-narrow.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <div class="container">
+      <div class="header">
+        <h3 class="text-muted">MeuBandoApp!!!</h3>
+      </div>
+
+      <div class="jumbotron">
+        <h1>Cadastro efetuado!</h1>
+        <p class="lead">Instale o MeuBandoApp no seu smartphone e começar a usar agora mesmo!</p>
+		<p><a class="btn btn-success" href="home.php" role="button">MeuBandoApp para <strong>IOS</strong></a></p>
+		<p><a class="btn btn-success" href="home.php" role="button">MeuBandoApp para <strong>Android</strong></a></p>
+		<p><a class="btn btn-link" href="home.php">Voltar para nossa Homepage</a></p>
+      </div>
+
+      <div class="row marketing">
+	  
+	   <div class="col-lg-6">
+		
+		  <h4>Você no bando!</h4>
+          <p>Crie seu perfil, adicione uma foto e saia usando. Contribua com seu motoclube e vá rodar com seu bando. Divirta-se!</p>
+		
+          <h4>Tesouraria</h4>
+          <p>Controle suas dívidas com seu bando. Confira o que deixou de pagar no bar, saiba como estão suas mensalidades e fique em dia!</p>
+
+          <h4>Todo mundo!</h4>
+          <p>Indique o MeuBandoApp para membros de outros motoclubes e tenha todo mundo aqui!</p>
+        </div>
+	  
+        <div class="col-lg-6">
+          <h4>Eventos</h4>
+          <p>Veja os eventos que estão rolando, suba na sua moto e vá fazer a festa. Não se esqueça de convidar os amigos!</p>
+
+          <h4>Comandas</h4>
+          <p>Evite sustos e situações contrangedoras. Confira tudo o que foi cadastrado em sua comanda e aproveite o evento.</p>
+
+          <h4>Tabela de preços</h4>
+          <p>Saiba quais são as opções de coisas pra beber ou comer, e confira se sua grana vai dar.</p>
+        </div>
+
+       
+      </div>
+
+      <div class="footer">
+        <p>&copy; MeuBandoApp 2014</p>
+      </div>
+
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+  </body>
+</html>
